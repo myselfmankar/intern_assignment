7d0f133e6ceb0577fb05e2abcf6964a3e0da31ca
myselfmankar <pict.vaishnav@gmail.com>
2025-04-24 15:28:49 +0530

    light weight puppeteer

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const path = require("path");
 const app = express();
 const port = 3000;
